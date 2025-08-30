91225f95433d63c63d78faf42c9dfb8609c62ac8
RaphCoder13 <9462038+RaphCoder13@users.noreply.github.com>
2025-08-30 18:15:23 +0200

    some changes on keymap

--- users/raphcoder13/common_keymap_defs_colemak-fr.h
+++ users/raphcoder13/common_keymap_defs_colemak-fr.h
@@ -104,14 +104,15 @@
 const custom_shift_key_t custom_shift_keys[] = {
     {FR_AMPR, FR_PIPE, true},           // Shift & is |
     {FR_QUOT, FR_DQUO, false},          // Shift ' is "
-    {FR_LPRN, FR_RPRN, false},          // Shift ( is )
+    {FR_LPRN, FR_SCLN, false},          // Shift ( is )
+    // {FR_LPRN, FR_RPRN, false},          // Shift ( is )
     {FR_RPRN, FR_DQUO, false},          // Shift ) is "
     {FR_BSLS, FR_DEG, true},            // Shift \ is °
     {FR_LABK, ALGR(KC_G), false},       // Shift < is ’
     {FR_RABK, FR_GRV, false},           // Shift > is `
     {FR_ASTR, FR_SLSH, true},           // Shift * is /
     {FR_AT, FR_MICR, true},             // Shift @ is μ
-    {FR_EXLM, FR_SUP2, true},           // Shift ! is ²
+    {FR_EXLM, FR_SUP2, true},           // Shift ! is qs²
     {ALGR(FR_CCED), S(FR_HASH), false}, // Shift ^ is ˘
     {FR_HASH, FR_TILD, false},          // Shift # is ~
     {FR_DAQ, ALGR(FR_X), false},        // Shift « is »
@@ -120,20 +121,26 @@ const custom_shift_key_t custom_shift_keys[] = {
     {FR_EGRV, SAGR(FR_EGRV), false},    // Shift è is È
     {FR_EACU, SAGR(FR_EACU), false},    // Shift é is É
     {FR_CCED, SAGR(FR_CCED), false},    // Shift ç is Ç
-    {FR_PERC, FR_MINS, false},
-    {FR_EXLM, FR_UNDS, false},
+    {FR_PERC, FR_QUES, false},
+    // {FR_EXLM, FR_UNDS, false},
+    // {S(FR_SCLN), FR_UNDS, false},       // Shift . is _
     {S(FR_SCLN), FR_COLN, false},       // Shift . is :
     // {S(FR_SCLN), FR_SCLN, true}      // Shift . is ;
     {FR_COMM, FR_SCLN, false},          // Shift , is ;
     // {FR_QUOT, S(FR_COMM), true},     // Shift ' is ?
-    {S(FR_SLSH), FR_BSLS, true},        // Shift / is anti-slash
+    // {S(FR_SLSH), FR_BSLS, true},        // Shift / is anti-slash
 
     // {S(FR_SLSH), S(FR_COMM), true},  // Shift / is ?
-    {FR_MINS, S(FR_COMM), true},        // Shift - is ?
+    // {FR_MINS, S(FR_COMM), true},        // Shift - is ?
+    {FR_MINS, FR_SLSH, true},        // Shift - is ?
     // {FR_MINS, FR_UNDS, true},        // Shift - is _
-    {FR_QUES, FR_EXLM, true},           // Shift - is _
+    // {FR_QUES, FR_EXLM, true},           // Shift - is _
 
-    {FR_UNDS, FR_SLSH, true},           // Shift _ is slash
+    {FR_UNDS, FR_QUES, false},           // Shift _ is ?
+
+    // {FR_UNDS, FR_SLSH, true},           // Shift _ is slash
+    // {FR_SLSH, FR_UNDS, true},           // Shift / is _
+    // {FR_SLSH, FR_COLN, true},           // Shift / is :
     // {FR_UNDS, FR_MINS, false},       // Shift _ is -
 
 };
@@ -144,15 +151,14 @@ uint8_t NUM_CUSTOM_SHIFT_KEYS =
 #define ________COLEMAK_FR_1DK_L1________        FR_Q,            FR_Z,             FR_F,           FR_P,           FR_B
 #define ________COLEMAK_FR_1DK_L2________        HOME_A_GUI,      HOME_R_ALT,       HOME_S_SFT,     HOME_T_CTL,     FR_G
 #define ________COLEMAK_FR_1DK_L3________        FR_W,            FR_X,             FR_C,           HOME_D_RALT,    FR_V
-
 #define ________COLEMAK_FR_1DK_R1________        FR_J,            FR_L,            FR_O,           OSL(ACCENT_COLEMAK_LAYER),     FR_Y
 #define ________COLEMAK_FR_1DK_R2________        FR_M,            HOME_N_CTL,      HOME_E_SFT,     HOME_I_ALT,      HOME_U_GUI
-#define ________COLEMAK_FR_1DK_R3________        FR_K,            HOME_H_RALT,     FR_MINS,        FR_COMM,         FR_DOT
+// #define ________COLEMAK_FR_1DK_R3________        FR_K,            HOME_H_RALT,     FR_MINS,        FR_COMM,         FR_DOT
+#define ________COLEMAK_FR_1DK_R3________        FR_K,            HOME_H_RALT,     FR_COMM,         FR_DOT,        FR_MINS
 
 #define ___________ACCENT_NEW_COLEMAK_L1___________          FR_ACRC,            FR_ITREM,         FR_ICRC,      FR_UCRC,    FR_QUOT
 #define ___________ACCENT_NEW_COLEMAK_L2___________          FR_AGRV,            ETREM_MAJ,        FR_ECRC,      FR_DQUO,    FR_AMPR
 #define ___________ACCENT_NEW_COLEMAK_L3___________          FR_AE,              FR_EURO,          FR_CCED,      FR_CIRC,    FR_BSLS
-
 #define ___________ACCENT_NEW_COLEMAK_R1___________          FR_OE,            KC_NO,            FR_OCRC,        S(FR_CIRC),    SAGR(FR_EQL)
 #define ___________ACCENT_NEW_COLEMAK_R2___________          ALGR(KC_G),       FR_EACU,          FR_EGRV,        FR_MICR,       FR_UGRV
 #define ___________ACCENT_NEW_COLEMAK_R3___________          ALGR(FR_W),       ALGR(FR_X),       SAGR(FR_MINS),  SAGR(FR_COMM), FR_EXLM
@@ -163,7 +169,6 @@ uint8_t NUM_CUSTOM_SHIFT_KEYS =
 #define ____________QWERTY_L1____________        KC_Q,                 KC_W,               KC_E,           KC_R,            KC_T
 #define ____________QWERTY_L2____________        KC_A,                 KC_S,               KC_D,           KC_F,            KC_G
 #define ____________QWERTY_L3____________        KC_Z,                 KC_X,               KC_C,           KC_V,            KC_B
-
 #define ____________QWERTY_R1____________        KC_Y,                 KC_U,               KC_I,           KC_O,            KC_P
 #define ____________QWERTY_R2____________        KC_H,                 KC_J,               KC_K,           KC_L,            KC_SLSH
 #define ____________QWERTY_R3____________        KC_N,                 KC_M,               KC_COMM,        KC_SCLN,         KC_COLN
@@ -172,25 +177,26 @@ uint8_t NUM_CUSTOM_SHIFT_KEYS =
 #define ____________SYMBOL_L1____________        FR_AT,                FR_PERC,            FR_LABK,        FR_RABK,         FR_QUOT
 #define ____________SYMBOL_L2____________        FR_HASH,              FR_EXLM,            FR_LPRN,        FR_RPRN,         FR_EQL
 #define ____________SYMBOL_L3____________        ALGR(FR_CCED),        FR_AMPR,            FR_ASTR,        FR_BSLS,         FR_DLR
-
-#define ____________SYMBOL_R1____________        FR_DQUO,              FR_LBRC,            FR_RBRC,        KC_NO,         KC_NO
+#define ____________SYMBOL_R1____________        FR_DQUO,              FR_LBRC,            FR_RBRC,        OSM(MOD_RALT),   KC_NO
 #define ____________SYMBOL_R2____________        FR_GRV,               OSM(MOD_RCTL),      TD(TD_OSM_RSFT),  OSM(MOD_LALT),   OSM(MOD_RGUI)
-#define ____________SYMBOL_R3____________        FR_QUOT,              FR_LCBR,            FR_RCBR,        OSM(MOD_RALT),         KC_NO
+#define ____________SYMBOL_R3____________        FR_QUOT,              FR_LCBR,            FR_RCBR,        FR_SCLN,         KC_NO
 
 #define ____________NUMBS_L1____________         KC_PSLS,              FR_7,               FR_8,           FR_9,            KC_PMNS
-#define ____________NUMBS_L2____________         KC_PAST,              FR_4,               FR_5,           FR_6,            KC_PPLS
-#define ____________NUMBS_L3____________         FR_0,                 FR_1,               FR_2,           FR_3,            KC_EQL
+#define ____________NUMBS_L2____________         FR_0,                 FR_4,               FR_5,           FR_6,            KC_PPLS
+#define ____________NUMBS_L3____________         KC_PAST,              FR_1,               FR_2,           FR_3,            KC_EQL
 #define ____________NUMBS_R1____________         KC_BSPC,              DM_RSTP,            DM_PLY1,        DM_PLY2,         KC_NO
 #define ____________NUMBS_R2____________         KC_DEL,               OSM(MOD_RCTL),      OSM(MOD_RSFT),  OSM(MOD_LALT),   OSM(MOD_RGUI)
 #define ____________NUMBS_R3____________         KC_NO,                OSM(MOD_RALT),      DM_REC1,        DM_REC2,         KC_NO
 
 #define ____________NAV_L1____________           C(FR_A),              USR_CUT,            USR_COPY,       USR_PASTE,       VIM_SYST_CLIPB
 #define ____________NAV_L2____________           OSM(MOD_LGUI),        OSM(MOD_LALT),      OSM(MOD_LSFT),  OSM(MOD_LCTL), USR_UNDO
-#define ____________NAV_L3____________           VIM_LAST_YANK,        LALT(KC_F4),        KC_APP,         OSM(MOD_RALT),   USR_REDO
-// #define ____________NAV_L3____________           VIM_LAST_YANK,        LALT(KC_F4),        LGUI(KC_LALT),  KC_APP,          C(FR_Y)
-#define ____________NAV_R1____________           KC_INS,               KC_HOME,            KC_UP,          KC_END,          KC_PGUP
-#define ____________NAV_R2____________           KC_PSCR,              KC_LEFT,            KC_DOWN,        KC_RGHT,         KC_PGDN
-#define ____________NAV_R3____________           KC_PAUS,              C(KC_LEFT),         C(KC_UP),       C(KC_RGHT),      KC_APP
+#define ____________NAV_L3____________           VIM_LAST_YANK,        LALT(KC_F4),        LCA(KC_LGUI),  OSM(MOD_RALT),   USR_REDO
+#define ____________NAV_R1____________           KC_INS,               KC_HOME,            KC_END,         KC_NO,           KC_NO
+#define ____________NAV_R2____________           KC_PSCR,              KC_LEFT,            KC_DOWN,        KC_UP,           KC_RIGHT
+#define ____________NAV_R3____________           KC_PAUS,              KC_PGUP,            KC_PGDN,        KC_NO,           KC_APP
+// #define ____________NAV_R1____________           KC_INS,               KC_HOME,            KC_UP,          KC_END,          KC_PGUP
+// #define ____________NAV_R2____________           KC_PSCR,              KC_LEFT,            KC_DOWN,        KC_RGHT,         KC_PGDN
+// #define ____________NAV_R3____________           KC_PAUS,              C(KC_LEFT),         C(KC_UP),       C(KC_RGHT),      KC_APP
 
 #define ____________FUN_L1____________           KC_F12,               KC_F7,              KC_F8,          KC_F9,           KC_SCRL
 #define ____________FUN_L2____________           KC_F11,               KC_F4,              KC_F5,          KC_F6,           KC_PSCR
@@ -207,17 +213,20 @@ uint8_t NUM_CUSTOM_SHIFT_KEYS =
 #define ____________APPS_R3___________           MEH(KC_K),            LCA(KC_F),          KC_CALC,        SGUI(KC_S),      KC_NO
 
 
-#define ___________MOUSE_L1___________           KC_NO,                USR_CUT,            USR_COPY,       USR_PASTE,       KC_NO
+#define ___________MOUSE_L1___________           KC_NO,                USR_CUT,            USR_COPY,       USR_PASTE,       MOUSE_TOGGLE
 #define ___________MOUSE_L2___________           OSM(MOD_LGUI),        OSM(MOD_LALT),      OSM(MOD_LSFT),  OSM(MOD_LCTL),   KC_ESC
-#define ___________MOUSE_L3___________           KC_NO,                KC_RALT,            KC_NO,          KC_MS_BTN4,      KC_MS_BTN5
-#define ___________MOUSE_R1___________           KC_MS_WH_UP,          KC_MS_LEFT,         KC_MS_DOWN,       KC_MS_UP,      KC_MS_RIGHT
-#define ___________MOUSE_R2___________           KC_MS_WH_DOWN,        OSM(MOD_RCTL),      OSM(MOD_RSFT),  OSM(MOD_LALT),   OSM(MOD_RGUI)
-#define ___________MOUSE_R3___________           KC_NO,                KC_MS_WH_LEFT,      KC_MS_WH_RIGHT, KC_RALT,         KC_NO
+#define ___________MOUSE_L3___________           KC_NO,                KC_RALT,            K_VSCROLL,      KC_MS_BTN4,      KC_MS_BTN5
+#define ___________MOUSE_R1___________           KC_NO,                KC_MS_WH_UP,        KC_MS_UP,       KC_MS_WH_DOWN,   KC_NO
+#define ___________MOUSE_R2___________           KC_NO,                KC_MS_LEFT,         KC_MS_DOWN,     KC_MS_RIGHT,     KC_NO
+#define ___________MOUSE_R3___________           KC_NO,                KC_MS_WH_LEFT,      KC_NO,          KC_MS_WH_RIGHT,  KC_NO
+// #define ___________MOUSE_R1___________           KC_MS_WH_UP,          KC_MS_LEFT,         KC_MS_DOWN,       KC_MS_UP,      KC_MS_RIGHT
+// #define ___________MOUSE_R2___________           KC_MS_WH_DOWN,        OSM(MOD_RCTL),      OSM(MOD_RSFT),  OSM(MOD_LALT),   OSM(MOD_RGUI)
+// #define ___________MOUSE_R3___________           KC_NO,                KC_MS_WH_LEFT,      KC_MS_WH_RIGHT, KC_RALT,         KC_NO
 
 
 #define ___________EXCEL_L1___________           KC_LEFT,              KC_DOWN,            KC_UP,          KC_RIGHT,        KC_F2
 #define ___________EXCEL_L2___________           OSM(MOD_LGUI),        OSM(MOD_LALT),      TD(TD_OSM_LSFT),  OSM(MOD_LCTL),   S(KC_G)
 #define ___________EXCEL_L3___________           KC_NO,                USR_CUT,            USR_COPY,       OSM(MOD_RALT),   USR_PASTE
 #define ___________EXCEL_R1___________           KC_PSLS,              KC_P7,              KC_P8,          KC_P9,           KC_PMNS
-#define ___________EXCEL_R2___________           KC_PAST,              KC_P4,              KC_P5,          KC_P6,           KC_PPLS
-#define ___________EXCEL_R3___________           KC_P0,                KC_P1,              KC_P2,          KC_P3,           KC_PDOT
+#define ___________EXCEL_R2___________           KC_P0,                KC_P4,              KC_P5,          KC_P6,           KC_PPLS
+#define ___________EXCEL_R3___________           KC_PAST,              KC_P1,              KC_P2,          KC_P3,           KC_PDOT
